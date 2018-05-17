f58ef3819a5789a478b28efff3ee7d4ca3149973
wanching_97 <wanching_97@hotmail.com>
2018-05-17 22:51:27 +0800

    customize admin

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,6 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False) # saved to database for the first time
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    # show the first column (in string) in admin
+    # show the first column (in string) in admin, if not, it displays Post object
     def __str__(self): 
         return self.title
