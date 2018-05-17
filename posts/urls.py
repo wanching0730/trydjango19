9dcd298e863fa18dbded52b4f526d927da735d17
wanching_97 <wanching_97@hotmail.com>
2018-05-18 00:18:16 +0800

    added urls for crud of post

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
+# refer to the first come url
 urlpatterns = [
-    url(r'^$', "posts.views.post_home"),
+    url(r'^$', "posts.views.post_list"),
+    url(r'^create/$', "posts.views.post_create"),
+    url(r'^detail/$', "posts.views.post_detail"),
+    url(r'^update/$', "posts.views.post_update"),
+    url(r'^delete/$', "posts.views.post_delete"),
+
 ]
