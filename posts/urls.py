f877361f6a12421788e4d15d4432d1854bc78d07
wanching_97 <wanching_97@hotmail.com>
2018-05-18 00:31:55 +0800

    shorten post urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from .views import (
+    post_create,
+    post_delete,
+    post_detail,
+    post_list,
+    post_update
+)
 
 # refer to the first come url
 urlpatterns = [
-    url(r'^$', "posts.views.post_list"),
-    url(r'^create/$', "posts.views.post_create"),
-    url(r'^detail/$', "posts.views.post_detail"),
-    url(r'^update/$', "posts.views.post_update"),
-    url(r'^delete/$', "posts.views.post_delete"),
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 
 ]
