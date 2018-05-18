dc774b722b0cdcfffbe0d336cbc3944f41b7e1f2
wanching_97 <wanching_97@hotmail.com>
2018-05-18 12:04:21 +0800

    added detail and list views

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Post
 
 # Create your views here.
 # functional view receive request, and send response
@@ -7,11 +9,21 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, id=1)
+    # variable to be passed to the view
+    context = {
+        "title": instance.title,
+        "instance": instance,
+    }
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
-    return render(request, "index.html", {})
-    # return HttpResponse("<h1>List</h1>")
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
